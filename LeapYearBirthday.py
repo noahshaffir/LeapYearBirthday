b48ca60822f246eaf4a7ec57abfbc274233cf248
Noah Shaffir <92744167+noahshaffir@users.noreply.github.com>
2022-02-28 12:38:52 -0500

    Update LeapYearBirthday.py

--- LeapYearBirthday.py
+++ LeapYearBirthday.py
@@ -3,7 +3,7 @@ You guys only have a birthday every 4 years, unlike everyone else.
 Please input your year of birth."""))
 current_year = int(input("Please enter the current year."))
 age = 0
-while current_year > 0 and current_year > year:
+while current_year > year:
     if current_year%4!=0:
         current_year-=1
     else:
